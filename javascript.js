cab38cf03da257a17cfffcafb9de93027d126c25
IcreateHtml <132458865+IcreateHtml@users.noreply.github.com>
2023-05-04 16:43:10 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,17 +1,1 @@
-function saveText() {
-        var e = document.getElementById("text").value,
-          t = new Blob([e], {
-            type: "text/plain"
-          }),
-          n = document.createElement("a");
-        n.download = "SavedFileInNotepad.txt", n.innerHTML = "Download File", null != window.webkitURL ? n.href = window.webkitURL.createObjectURL(t) : (n.href = window.URL.createObjectURL(t), n.onclick = destroyClickedElement, n.style.display = "none", document.body.appendChild(n)), n.click()
-      }
 
-      function uploadFile() {
-        document.getElementById("fileInput").click(), document.getElementById("fileInput").addEventListener("change", function() {
-          var e = new FileReader;
-          e.onload = function() {
-            document.getElementById("text").value = e.result
-          }, e.readAsText(document.getElementById("fileInput").files[0])
-        })
-      }
